5a2c3498d57643878c4c7dc73d9610e42f2fc742
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-02 03:43:17 +0530

    Rendering HTML properly

--- basic_http_server/index.js
+++ basic_http_server/index.js
@@ -1,11 +1,21 @@
 const http = require('http');
 const port = 8000;
-
+const fs =  require('fs');
 
 function reqHandler(request, response){
     console.log(request.url);
     response.writeHead(200, {'content-type': 'text-html'})
-    response.end('<h1>Gotcha Render HTML!!!</h1>');
+    
+    fs.readFile('./index.html', function(err, data){
+        if(err){
+            console.log('error', err);
+            return response.end('<h1>Error !</h1>');
+        }
+
+        return response.end(data);
+    });
+
+    // response.end('<h1>Gotcha Render HTML!!!</h1>');
 }
 
 const server = http.createServer(reqHandler);
