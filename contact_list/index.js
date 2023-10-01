ab7dedd20e5a39edcd424bc3c7ddad55a9412748
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-02 05:24:38 +0530

    Sending 1st HTML  Response Express

--- contact_list/index.js
+++ contact_list/index.js
@@ -3,12 +3,15 @@ const port = 8000;
 
 const app = express();
 
+
+app.get('/home', function(req, res){
+    res.send('<h1>Express Home</h1>')
+});
+
 app.listen(port, function(err){
 
     if(err){
         console.log("Error in running the server", err);
     }
-
     console.log("Express running on port: ", port);
-
 });
