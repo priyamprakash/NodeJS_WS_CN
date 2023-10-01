bc3884873a7916df5114c6fa87dbbac101de9e0c
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-02 04:24:35 +0530

    Switch case for file response

--- basic_http_server/index.js
+++ basic_http_server/index.js
@@ -5,8 +5,22 @@ const fs =  require('fs');
 function reqHandler(request, response){
     console.log(request.url);
     response.writeHead(200, {'content-type': 'text-html'})
-    
-    fs.readFile('./index.html', function(err, data){
+
+    let filePath;
+
+    switch(request.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        default :
+            filePath = './404.html'
+            
+        }
+
+    fs.readFile(filePath, function(err, data){
         if(err){
             console.log('error', err);
             return response.end('<h1>Error !</h1>');
