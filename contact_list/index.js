a15de6b5f0fddbba2dbe4a24ba10c53fa2023f1c
Priyam Prakash <65361533+priyamprakash@users.noreply.github.com>
2023-10-02 06:03:03 +0530

    EJS install | Render Home.ejs properly

--- contact_list/index.js
+++ contact_list/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/home', function(req, res){
-    res.send('<h1>Express Home</h1>')
+
+app.get('/', function(req, res){
+    return res.render('home');
 });
 
 app.listen(port, function(err){
